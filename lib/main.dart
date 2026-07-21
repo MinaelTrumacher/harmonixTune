5afafd9202a8e2538264ad345e0e7aae4b1b12a8
MinaelTrumacher <truong.minhtuan.tlse@gmail.com>
2026-07-21 10:08:42 +0200

    US2 - flag dev prod stage

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,7 @@
-import 'dart:async';
+import 'bootstrap.dart';
+import 'core/config/flavor.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_performance/firebase_performance.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-
-import 'data/models/tuning_profile_model.dart';
-import 'data/repositories/tuning_profile_repository_impl.dart';
-import 'firebase_options.dart';
-import 'presentation/app.dart';
-
-Future<void> main() async {
-  runZonedGuarded(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
-      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
-  await Hive.initFlutter();
-  Hive.registerAdapter(TuningProfileModelAdapter());
-  final tuningProfileBox = await Hive.openBox<TuningProfileModel>(
-    'tuning_profiles',
-  );
-
-
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-      await FirebasePerformance.instance.setPerformanceCollectionEnabled(
-        !kDebugMode,
-      );
-      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(
-        !kDebugMode,
-      );
-
-      FlutterError.onError =
-          FirebaseCrashlytics.instance.recordFlutterFatalError;
-      PlatformDispatcher.instance.onError = (error, stack) {
-        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-        return true;
-      };
-
-      runApp(
-    HarmonixTuneApp(
-      tuningProfileRepository: TuningProfileRepositoryImpl(tuningProfileBox),
-    ),
-  );
-    },
-    (error, stack) =>
-        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
-  );
-}
+// Alias de confort pour `flutter run` sans -t : équivaut à main_prod.dart.
+// Une fois les flavors Android en place, utiliser explicitement
+// --flavor <dev|staging|prod> -t lib/main_<flavor>.dart.
+Future<void> main() => bootstrap(Flavor.prod);
